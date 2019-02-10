c15e41a429700b8bfdec5212240b14eeabb845dd
John Teske <john.teske@gmail.com>
2019-02-10 12:32:46 -0800

    Encapsulate jackEventWatcher, set callbacks from config

--- hammerspoon/jackEventWatcher.lua
+++ hammerspoon/jackEventWatcher.lua
@@ -1,27 +1,25 @@
--- Prevent blaring audio if jack unplugged
-local function headphones_disconnected(audiodevice)
-    hs.notify.show("Headphones unplugged", "External speakers muted", "")
-    audiodevice:setOutputMuted(true)
-end
+local function connectedCallback() end
+local function disconnectedCallback() end
 
--- Start headphone volume at a safe level
-local function headphones_connected(audiodevice)
-    hs.notify.show("Headphones plugged", "Volume set to 20%", "")
-    audiodevice:setOutputVolume(20)
-end
-
--- Per-device watcher to detect headphones in/out
 local function audiodeviceWatch(dev_uid, event_name, _, _)
     local device = hs.audiodevice.findDeviceByUID(dev_uid)
     if event_name == 'jack' then
         if device:jackConnected() then
-            headphones_connected(device)
+            connectedCallback(device)
         else
-            headphones_disconnected(device)
+            disconnectedCallback(device)
         end
     end
 end
 
-for _, dev in ipairs(hs.audiodevice.allOutputDevices()) do
-    dev:watcherCallback(audiodeviceWatch):watcherStart()
+local function start ()
+    for _, dev in ipairs(hs.audiodevice.allOutputDevices()) do
+        dev:watcherCallback(audiodeviceWatch):watcherStart()
+    end
+end
+
+return function (command, value)
+    if command == "start" then start() end
+    if command == "setConnectedCallback" then connectedCallback = value end
+    if command == "setDisconnectedCallback" then disconnectedCallback = value end
 end
