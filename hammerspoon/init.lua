7cabe5e44bf0e3280990e72b983d256c7544cb2c
John Teske <john.teske@gmail.com>
2019-02-10 12:32:46 -0800

    Encapsulate jackEventWatcher, set callbacks from config

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,5 +1,21 @@
 require("split")
-require("jackEventWatcher")
+
+--
+-- Audio
+--
+
+local jackEventWatcher = require("jackEventWatcher")
+
+local function volumeSetter (level, message)
+    return function (device)
+        device:setOutputVolume(level)
+        hs.notify.show(message, string.format("Volume set to %d%%", level), "")
+    end
+end
+
+jackEventWatcher("setConnectedCallback", volumeSetter(20, "Headphones connected"))
+jackEventWatcher("setDisconnectedCallback", volumeSetter(0, "Headphones disconnected"))
+jackEventWatcher("start")
 
 --
 -- Config
@@ -8,4 +24,5 @@ require("jackEventWatcher")
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
     hs.reload()
 end)
+
 hs.alert.show("Config loaded")
