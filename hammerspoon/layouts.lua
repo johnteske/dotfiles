24f88c5251d765f5e64f33d49204b9af1caf9d08
John Teske <john.teske@gmail.com>
2019-02-11 08:26:59 -0800

    Add closeAll to layouts.lua
    Also refactor to be more functional

--- hammerspoon/layouts.lua
+++ hammerspoon/layouts.lua
@@ -1,8 +1,22 @@
 local layoutMenu
 
-local menuItems = {
+local function closeAll ()
+    for _, app in ipairs(hs.application.runningApplications()) do
+        -- kill all apps that exist in the dock
+        if app:kind() == 1 then app:kill() end
+    end
+end
+
+local preMenuItems = {
     { title = "Layouts", disabled = true },
     { title = "-" }
+}
+
+local menuItems = {}
+
+local postMenuItems = {
+    { title = "-" },
+    { title = "Close all", fn = closeAll }
 }
 
 local function create ()
@@ -12,7 +26,26 @@ local function create ()
     layoutMenu:setMenu(menuItems)
 end
 
+local function copy (t, target)
+    local c = target or {}
+    for _, item in ipairs(t) do c[#c + 1] = item end
+    return c
+end
+
+local function cat (a, b)
+    local c = copy(a)
+    return copy(b, c)
+end
+
+local function setMenu ()
+    local t = cat({}, preMenuItems)
+    t = cat(t, menuItems)
+    t = cat(t, postMenuItems)
+
+    layoutMenu:setMenu(t)
+end
+
 return function (command, value)
     if command == "create" then create() end
-    if command == "add" then table.insert(menuItems, value); layoutMenu:setMenu(menuItems) end
+    if command == "add" then table.insert(menuItems, value); setMenu() end
 end
