0cf5e6c8b14150c4f697064fbe51f8846861d537
John Teske <john.teske@gmail.com>
2019-02-12 08:16:12 -0800

    Add VSCode key repeat default

--- install.sh
+++ install.sh
@@ -14,27 +14,28 @@ bak_lns ~/.aliases ~/dotfiles/bash/aliases
 bak_lns ~/.prompt ~/dotfiles/bash/prompt
 bak_lns ~/.git_utils ~/dotfiles/bash/git_utils
 
+bak_lns ~/.vimrc ~/dotfiles/vim/vimrc
+
 if [ "$INSTALL_ENV" == "linux" ]; then
     bak_lns ~/.bashrc.linux ~/dotfiles/bash/bashrc.linux
 fi
 
-ln_hammerspoon () {
-    for f in ~/dotfiles/hammerspoon/*.lua; do
-        file=${f##*/}
-        ln -sf $f ~/.hammerspoon/$file
-    done
-}
+if [ "$INSTALL_ENV" != "osx" ]; then
+    source ~/.bashrc
+fi
 
 if [ "$INSTALL_ENV" == "osx" ]; then
     bak_lns ~/.bash_profile ~/dotfiles/bash/bash_profile.osx
     source ~/.bash_profile
 
-    ln_hammerspoon
-else
-    source ~/.bashrc
-fi
+    # vscodevim.vim
+    defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
 
-bak_lns ~/.vimrc ~/dotfiles/vim/vimrc
+    for f in ~/dotfiles/hammerspoon/*.lua; do
+        file=${f##*/}
+        ln -sf $f ~/.hammerspoon/$file
+    done
+fi
 
 #if [ ! -f ~/.git-completion.bash ]; then
 #    echo -n "Install git completion? [Y/n] "; read answer
