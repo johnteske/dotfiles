c4407a5da2c5bb42fdd1959cbf53c90f5db342e7
John Teske <john.teske@gmail.com>
2019-03-18 23:42:00 -0700

    Use command fzf preview and window commands

--- bash/fuzzy.bash
+++ bash/fuzzy.bash
@@ -2,10 +2,13 @@
 
 export FZF_DEFAULT_OPTS='--no-mouse'
 
+preview='head -80'
+preview_window='--preview-window right'
+
 gcf () {
-    git checkout "$(git branch | cut -c 3- | fzf --header 'git checkout')"
+  git checkout "$(git branch | cut -c 3- | fzf --header 'git checkout' --preview "git show {} | ${preview}" ${preview_window})"
 }
 
 vf () {
-   vim "$(fzf --header 'vim')"
+  vim "$(fzf --header 'vim' --preview "${preview} {}" ${preview_window})"
 }
