96d050af51cf339d7fec9013ab294d64e073df16
John Teske <john.teske@gmail.com>
2019-03-19 07:39:32 -0700

    Allow clean exit from fuzzy commands

--- bash/fuzzy.bash
+++ bash/fuzzy.bash
@@ -6,9 +6,11 @@ preview='head -80'
 preview_window='--preview-window right'
 
 gcf () {
-  git checkout "$(git branch | cut -c 3- | fzf --header 'git checkout' --preview "git show {} | ${preview}" ${preview_window})"
+  local b=$(git branch | cut -c 3- | fzf --header 'git checkout' --preview "git show {} | ${preview}" ${preview_window})
+  [ -f "$b" ] && git checkout "$b"
 }
 
 vf () {
-  vim "$(fzf --header 'vim' --preview "${preview} {}" ${preview_window})"
+  local f=$(fzf --header 'vim' --preview "${preview} {}" ${preview_window})
+  [ -f "$f" ] && vim "$f"
 }
