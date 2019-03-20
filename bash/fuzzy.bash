5b34a987180c97e140276ab85eaa6f007e3667b6
John Teske <john@98point6.com>
2019-03-20 13:26:28 -0700

    Check if git branch is empty, not if file exists

--- bash/fuzzy.bash
+++ bash/fuzzy.bash
@@ -6,8 +6,9 @@ preview='head -80'
 preview_window='--preview-window right'
 
 gcf () {
-  local b=$(git branch | cut -c 3- | fzf --header 'git checkout' --preview "git show {} | ${preview}" ${preview_window})
-  [ -f "$b" ] && git checkout "$b"
+  local b
+  b=$(git branch | cut -c 3- | fzf --header 'git checkout' --preview "git show {} | ${preview}" ${preview_window})
+  [ -n "$b" ] && git checkout "$b"
 }
 
 vf () {
