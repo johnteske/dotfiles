2cafae48b01d60dedcf197ba69d6a0b844f0749c
John Teske <john@98point6.com>
2019-03-22 14:42:35 -0700

    Keep git checkout fuzzy in home row (git fuzz)

--- bash/fuzzy.bash
+++ bash/fuzzy.bash
@@ -5,7 +5,7 @@ export FZF_DEFAULT_OPTS='--no-mouse'
 preview='head -80'
 preview_window='--preview-window right'
 
-gcf () {
+gf () {
   local b
   b=$(git branch | cut -c 3- | fzf --header 'git checkout' --preview "git show {} | ${preview}" ${preview_window})
   [ -n "$b" ] && git checkout "$b"
