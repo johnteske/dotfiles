52db710baa323cb0334b2d3c0f62329c93a165bd
John Teske <john.teske@gmail.com>
2019-03-26 21:58:09 -0700

    Use fd in fzf if installed

--- bash/fuzzy.bash
+++ bash/fuzzy.bash
@@ -2,16 +2,27 @@
 
 export FZF_DEFAULT_OPTS='--no-mouse'
 
+if builtin type -P fd &> /dev/null; then
+  export FZF_DEFAULT_COMMAND='fd --type f'
+fi
+
 preview='head -80'
-preview_window='--preview-window right'
+preview_window='right'
 
 gf () {
   local b
-  b=$(git branch | cut -c 3- | fzf --header 'git checkout' --preview "git show {} | ${preview}" ${preview_window})
+  b=$(git branch | cut -c 3- | fzf \
+    --header 'git checkout' \
+    --preview "git show {} | ${preview}" \
+    --preview-window ${preview_window})
   [ -n "$b" ] && git checkout "$b"
 }
 
 vf () {
-  local f=$(fzf --header 'vim' --preview "${preview} {}" ${preview_window})
+  local f
+  f=$(fzf \
+    --header 'vim' \
+    --preview "${preview} {}" \
+    --preview-window "${preview_window}")
   [ -f "$f" ] && vim "$f"
 }
