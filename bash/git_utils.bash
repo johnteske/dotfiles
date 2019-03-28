d17a56c326a1eebbec0c91df951150850cd541ea
John Teske <john.teske@gmail.com>
2019-03-27 18:09:52 -0700

    Share common commands between files

--- bash/git_utils.bash
+++ bash/git_utils.bash
@@ -3,7 +3,7 @@
 # Git completion
 [ -f ~/.git-completion.bash ] && source ~/.git-completion.bash
 
-git-branch-current () {
+__jt_git_branch () {
     git rev-parse --abbrev-ref HEAD
 }
 
@@ -12,7 +12,7 @@ gp () {
     git push 2>&1 | tee "$tmp"
 
     # If git suggests to push and set upstream, give a choice to run
-    push_set="git push --set-upstream origin $(git-branch-current)"
+    push_set="git push --set-upstream origin $(__jt_git_branch)"
     if grep -q "${push_set}" "$tmp"; then
         local answer
         echo -n "Push and set the remote as upstream? [y/N] "
@@ -26,7 +26,7 @@ gp () {
 
 git-select () {
     git for-each-ref --format="%(refname:short)" refs/heads/\* | \
-    grep -v "master\|$(git-branch-current)" | \
+    grep -v "master\|$(__jt_git_branch)" | \
     while read -r line; do
         read -rp "Select branch: $line? [y/N] " answer </dev/tty;
         case "$answer" in
