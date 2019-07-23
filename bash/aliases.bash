faffc4f61da1feacc1b8c895609722da22c7c857
John Teske <john.teske@protonmail.com>
2019-07-23 11:49:25 -0700

    Add git stash alias

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -12,6 +12,7 @@ alias g.="cd \$(git rev-parse --show-toplevel)"
 alias ga="git add -p"
 alias gs="git status"
 alias gd="git diff"
+alias gds="git diff --staged"
 alias gc="git checkout"
 alias gb="git branch"
 
