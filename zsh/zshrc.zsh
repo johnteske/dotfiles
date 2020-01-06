6348f7fb58d513df4f1fd9e706da974290c34010
John Teske <john.teske@protonmail.com>
2020-01-06 11:40:23 -0800

    Add indicator of normal mode in zsh

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -6,10 +6,22 @@ autoload -Uz vcs_info
 precmd_vcs_info() { vcs_info }
 precmd_functions+=( precmd_vcs_info )
 setopt prompt_subst
-RPROMPT=\$vcs_info_msg_0_
 zstyle ':vcs_info:git:*' formats '%F{black}%b%f'
-#zstyle ':vcs_info:git:*' formats '%F{240}(%b)%r%f'
 zstyle ':vcs_info:*' enable git
 
 #RPROMPT='%(?.%F{green}OK.%F{red}NO)%f'
+
 fpath+=${ZDOTDIR:-~}/.zsh_functions
+
+# vi mode
+bindkey -v
+
+export KEYTIMEOUT=1 # in tenths of a second
+
+function zle-line-init zle-keymap-select {
+  VIM_PROMPT='%F{green}[% NORMAL]% %b%f'
+  RPS1="${${KEYMAP/vicmd/$VIM_PROMPT}/(main|viins)/} $vcs_info_msg_0_ $EPS1"
+  zle reset-prompt
+}
+zle -N zle-line-init
+zle -N zle-keymap-select
