78d7e6ec2293e863e164237089cbe5043b963725
John Teske <john.teske@gmail.com>
2020-01-14 06:49:59 +0000

    Add fallbacks to other fuzzy functions

--- bash/fuzzy.bash
+++ bash/fuzzy.bash
@@ -3,26 +3,38 @@
 export FZF_DEFAULT_OPTS='--no-mouse --color 16'
 
 if command -v fd &> /dev/null; then
-  export FZF_DEFAULT_COMMAND='fd --type f'
+export FZF_DEFAULT_COMMAND='fd --type f'
 fi
 
 preview='head -80'
 preview_window='right'
 
-gf () {
+if command -v fzf >/dev/null
+then function gf {
   local b
-  b=$(git branch | cut -c 3- | fzf \
-    --header 'git checkout' \
-    --preview "git show {} | ${preview}" \
-    --preview-window ${preview_window})
-  [ -n "$b" ] && git checkout "$b"
+    b=$(git branch | cut -c 3- | fzf \
+        --header 'git checkout' \
+        --preview "git show {} | ${preview}" \
+        --preview-window ${preview_window})
+    [ -n "$b" ] && git checkout "$b"
 }
+else function gf {
+  echo "'fzf' not installed. Using fallback."
+  git branch
+}
+fi
 
-vf () {
+if command -v fzf >/dev/null
+then function vf () {
   local f
-  f=$(fzf \
-    --header 'vim' \
-    --preview "${preview} {}" \
-    --preview-window "${preview_window}")
-  [ -f "$f" ] && vim "$f"
+    f=$(fzf \
+        --header 'vim' \
+        --preview "${preview} {}" \
+        --preview-window "${preview_window}")
+    [ -f "$f" ] && vim "$f"
 }
+else function vf {
+  echo "'fzf' not installed. Using fallback."
+  vim .
+}
+fi
