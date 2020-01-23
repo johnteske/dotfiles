0a4ead3c59620ff2bf89d677740af9af1fd6c8ba
John Teske <john@98point6.com>
2020-01-23 15:34:10 -0800

    Make zsh prompt standout if not in main/insert mode

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,7 +1,5 @@
 [[ -f ~/.bashrc ]] && . ~/.bashrc
 
-PROMPT='%U%1~ %#%u '
-
 autoload -Uz vcs_info
 precmd_vcs_info() { vcs_info }
 precmd_functions+=( precmd_vcs_info )
@@ -9,18 +7,23 @@ setopt prompt_subst
 zstyle ':vcs_info:git:*' formats '%F{black}%b%f'
 zstyle ':vcs_info:*' enable git
 
-#RPROMPT='%(?.%F{green}OK.%F{red}NO)%f'
-
-fpath+=${ZDOTDIR:-~}/.zsh_functions
-
 # vi mode
 bindkey -v
 
 export KEYTIMEOUT=1 # in tenths of a second
 
 function zle-line-init zle-keymap-select {
-  VIM_PROMPT='%F{green}[% NORMAL]% %b%f'
-  RPS1="${${KEYMAP/vicmd/$VIM_PROMPT}/(main|viins)/} $vcs_info_msg_0_ $EPS1"
+  case ${KEYMAP} in
+    (vicmd)  PROMPT_STYLE='%S' ;; # add standout
+    (*)      PROMPT_STYLE='' ;;
+  esac
+
+  PROMPT="$PROMPT_STYLE%U%1~ %#%u%s "
+
+  RPS1=""
+  RPS1+="$vcs_info_msg_0_"
+  RPS1+="$EPS1"
+
   zle reset-prompt
 }
 zle -N zle-line-init
