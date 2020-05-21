8e09fad405757b3a262ab3ee3e60b33183600cb0
John Teske <john.teske@protonmail.com>
2020-05-21 08:59:13 -0700

    Use git aliases over bash aliases
    This allows 'dotf' and 'dotl' to use similar shorthand.

--- .dotfiles/aliases.bash
+++ .dotfiles/aliases.bash
@@ -7,15 +7,9 @@ alias ..="cd .."
 alias ll="ls -a"
 
 # git
-alias g.="cd \$(git rev-parse --show-toplevel)"
-alias ga="git add -p"
-alias gs="git status"
-alias gd="git diff"
-alias gds="git diff --staged"
-alias gc="git checkout"
-alias gb="git branch"
-alias gp="git-push"
-alias gl="git ls-files"
+alias g="git"
+#alias g.="cd \$(git rev-parse --show-toplevel)"
+#alias gp="git-push"
 
 # tmux
 alias t="tmux"
--- .dotfiles/git_utils.bash
+++ .dotfiles/git_utils.bash
@@ -4,3 +4,11 @@
 if [ "$SHELL" = "/bin/bash" ]; then
   [ -f ~/.git-completion.bash ] && source ~/.git-completion.bash
 fi
+
+git config --global alias.a 'add -p'
+git config --global alias.b branch
+git config --global alias.c checkout
+git config --global alias.d diff
+git config --global alias.ds 'diff --staged'
+git config --global alias.l ls-files
+git config --global alias.s status
