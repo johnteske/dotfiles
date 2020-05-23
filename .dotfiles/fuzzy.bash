92babd5ef9568fdb01ee7da1bc8c16ae4f6c7691
John Teske <john.teske@protonmail.com>
2020-05-23 16:42:37 -0700

    Use /bin/sh
    Avoid bashisms and also aim for dash support.

--- .dotfiles/fuzzy.bash
+++ .dotfiles/fuzzy.bash
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 export FZF_DEFAULT_OPTS='--no-mouse --color 16'
 
-if command -v fd &> /dev/null; then
+if command -v fd > /dev/null 2>&1; then
 export FZF_DEFAULT_COMMAND='fd --hidden --type file'
 fi
 
@@ -10,30 +10,28 @@ preview='head -80'
 preview_window='right'
 
 if command -v fzf >/dev/null
-then function gf {
-  local b
+then gf () {
     b=$(git branch | cut -c 3- | fzf \
         --header 'git checkout' \
         --preview "git show {} | ${preview}" \
         --preview-window ${preview_window})
     [ -n "$b" ] && git checkout "$b"
 }
-else function gf {
+else gf () {
   echo "'fzf' not installed. Using fallback."
   git branch
 }
 fi
 
 if command -v fzf >/dev/null
-then function vf () {
-  local f
+then vf () {
     f=$(fzf \
         --header 'vim' \
         --preview "${preview} {}" \
         --preview-window "${preview_window}")
     [ -f "$f" ] && vim "$f"
 }
-else function vf {
+else vf () {
   echo "'fzf' not installed. Using fallback."
   vim .
 }
