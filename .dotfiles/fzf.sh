df84d0bebb270b3f89e103412104ca5f07ffb871
John Teske <john.teske@protonmail.com>
2020-05-28 20:18:48 -0700

    Exit early if not on git branch

--- .dotfiles/fzf.sh
+++ .dotfiles/fzf.sh
@@ -15,7 +15,9 @@ if command -v fzf >/dev/null; then
   f() {
     case "$@" in
       g)
-        # TODO exit early if not git repo
+        # exit early if not git repo
+        if ! __jt__git-branch-name; then return 1; fi
+
         # TODO display current branch--and/or remove from list
         fzff=$(git branch | cut -c 3- | fzf \
         --header 'git checkout' \
