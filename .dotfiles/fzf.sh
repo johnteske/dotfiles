3ec0e35c7b17f9985b628ec9ac42d1f5555f011e
John Teske <john.teske@protonmail.com>
2020-05-28 23:15:09 -0700

    Display current branch in fzf header

--- .dotfiles/fzf.sh
+++ .dotfiles/fzf.sh
@@ -3,10 +3,11 @@
 # TODO handle no args (don't show header)
 # TODO how to handle (or not) stdin
 # TODO standardize preview (as FZF_DEFAULT_PREVIEW?)
+# TODO max preview height
 
 if command -v fzf >/dev/null; then
 
-  export FZF_DEFAULT_OPTS='--no-mouse --color 16 --preview "head -40 {}" --preview-window "right"'
+  export FZF_DEFAULT_OPTS='--no-mouse --color 16 --preview "head -40 {}" --info=hidden'
 
   if command -v fd > /dev/null 2>&1; then
     export FZF_DEFAULT_COMMAND='fd --hidden --exclude .git --type file'
@@ -16,12 +17,16 @@ if command -v fzf >/dev/null; then
     case "$@" in
       g)
         # exit early if not git repo
-        if ! __jt__git-branch-name; then return 1; fi
+        if ! __jt__git-branch-name >/dev/null; then return 1; fi
 
-        # TODO display current branch--and/or remove from list
-        fzff=$(git branch | cut -c 3- | fzf \
-        --header 'git checkout' \
-        --preview "git show {} | head -40")
+        # display current branch above header
+        fzff=$(git branch \
+          --format='%(refname:short)' \
+          --sort='-HEAD' | \
+        fzf \
+          --header-lines=1 \
+          --header 'git checkout' \
+          --preview 'git show {} | head -40')
         [ -n "$fzff" ] && git checkout "$fzff"
         ;;
       v)
