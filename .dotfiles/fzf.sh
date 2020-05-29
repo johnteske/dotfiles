be5b9a4310ddb55c9674b023b8b909fef72eeb36
John Teske <john.teske@protonmail.com>
2020-05-28 18:58:01 -0700

    Update f command to replace vf and gf

--- .dotfiles/fzf.sh
+++ .dotfiles/fzf.sh
@@ -1,52 +1,37 @@
 #!/bin/sh
 
-export FZF_DEFAULT_OPTS='--no-mouse --color 16'
+# TODO handle no args (don't show header)
+# TODO how to handle (or not) stdin
+# TODO standardize preview (as FZF_DEFAULT_PREVIEW?)
 
-if command -v fd > /dev/null 2>&1; then
-export FZF_DEFAULT_COMMAND='fd --hidden --exclude .git --type file'
-fi
+if command -v fzf >/dev/null; then
 
-preview='head -80'
-preview_window='right'
+  export FZF_DEFAULT_OPTS='--no-mouse --color 16 --preview "head -40 {}" --preview-window "right"'
 
-if command -v fzf >/dev/null
-then gf () {
-    b=$(git branch | cut -c 3- | fzf \
+  if command -v fd > /dev/null 2>&1; then
+    export FZF_DEFAULT_COMMAND='fd --hidden --exclude .git --type file'
+  fi
+
+  f() {
+    case "$@" in
+      g)
+        # TODO exit early if not git repo
+        # TODO display current branch--and/or remove from list
+        fzff=$(git branch | cut -c 3- | fzf \
         --header 'git checkout' \
-        --preview "git show {} | ${preview}" \
-        --preview-window ${preview_window})
-    [ -n "$b" ] && git checkout "$b"
-}
-else gf () {
-  git branch
-}
-fi
+        --preview "git show {} | head -40")
+        [ -n "$fzff" ] && git checkout "$fzff"
+        ;;
+      v)
+        fzff=$(fzf --header 'vim')
+        [ -f "$fzff" ] && vim "$fzff"
+        ;;
+      *)
+        fzff=$(fzf --header "$@")
+        [ -f "$fzff" ] && "$@" "$fzff"
+        ;;
+    esac
+    unset fzff
+  }
 
-if command -v fzf >/dev/null
-then vf () {
-    f=$(fzf \
-        --header 'vim' \
-        --preview "${preview} {}" \
-        --preview-window "${preview_window}")
-    [ -f "$f" ] && vim "$f"
-}
-
-f() {
-  case "$@" in
-    g)
-      git branch
-      ;;
-    v)
-      #shift
-      vim "$(fzf)" #"$@"
-      ;;
-    *)
-      "$@" "$(fzf)"
-      ;;
-  esac
-}
-
-else vf () {
-  vim .
-}
 fi
