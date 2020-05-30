9b335b2fcc3464774c0d86c2fc6378f836b5b900
John Teske <john.teske@protonmail.com>
2020-05-30 09:41:41 -0700

    Run shfmt -i=2 -w .dotfiles/fzf.sh

--- .dotfiles/fzf.sh
+++ .dotfiles/fzf.sh
@@ -9,39 +9,39 @@ if command -v fzf >/dev/null; then return 1; fi
 __jt__fzf_default_preview="head -40"
 export FZF_DEFAULT_OPTS="--no-mouse --color 16 --preview '$__jt__fzf_default_preview {}' --info=hidden"
 
-if command -v fd > /dev/null 2>&1; then
+if command -v fd >/dev/null 2>&1; then
   export FZF_DEFAULT_COMMAND='fd --hidden --exclude .git --type file'
 fi
 
 f() {
   case "$@" in
-    g)
-      # exit early if not git repo
-      if ! __jt__git-branch-name 1>/dev/null; then return 1; fi
+  g)
+    # exit early if not git repo
+    if ! __jt__git-branch-name 1>/dev/null; then return 1; fi
 
-      # display current branch above header
-      fzff=$(git branch \
-        --format='%(refname:short)' \
-        --sort='-HEAD' | \
-        fzf \
+    # display current branch above header
+    fzff=$(git branch \
+      --format='%(refname:short)' \
+      --sort='-HEAD' |
+      fzf \
         --header-lines=1 \
         --header 'git checkout' \
         --preview "git show {} | $__jt__fzf_default_preview")
-         [ -n "$fzff" ] && git checkout "$fzff"
-      ;;
-    v)
-      fzff=$(fzf --header 'vim')
-      [ -f "$fzff" ] && vim "$fzff"
-      ;;
-    *)
-      # handle args/no args
-      if [ "$#" -ne 0 ]; then
-        fzff=$(fzf --header "$@")
-        "$@" "$fzff"
-      else
-        fzf
-      fi
-      ;;
+    [ -n "$fzff" ] && git checkout "$fzff"
+    ;;
+  v)
+    fzff=$(fzf --header 'vim')
+    [ -f "$fzff" ] && vim "$fzff"
+    ;;
+  *)
+    # handle args/no args
+    if [ "$#" -ne 0 ]; then
+      fzff=$(fzf --header "$@")
+      "$@" "$fzff"
+    else
+      fzf
+    fi
+    ;;
   esac
   unset fzff
 }
