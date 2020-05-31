50b6c406eaf294a98606b9511a6fc5abb6e5b596
John Teske <john.teske@protonmail.com>
2020-05-31 16:55:40 -0700

    Add 'f' function, a composable fzf wrapper (#15)
    Also replace 'vf' and 'gf' with 'f v' and 'f g'.

--- .dotfiles/fzf.sh
+++ .dotfiles/fzf.sh
@@ -1,36 +1,55 @@
 #!/bin/sh
 
-export FZF_DEFAULT_OPTS='--no-mouse --color 16'
+# exit early if fzf not found
+if ! command -v fzf >/dev/null; then return 1; fi
 
-if command -v fd > /dev/null 2>&1; then
-export FZF_DEFAULT_COMMAND='fd --hidden --exclude .git --type file'
+__jt__fzf_default_preview="[ -f '{}' ] && head -60"
+export FZF_DEFAULT_OPTS="--no-mouse --color=16 --info=hidden --preview '$__jt__fzf_default_preview {}'"
+
+if command -v fd >/dev/null 2>&1; then
+  export FZF_DEFAULT_COMMAND='fd --hidden --exclude .git --type file'
 fi
 
-preview='head -80'
-preview_window='right'
+f() {
+  if [ ! -t 0 ]; then
+    cat <<EOF
+f: Use of stdin is not supported. Use fzf instead.
 
-if command -v fzf >/dev/null
-then gf () {
-    b=$(git branch | cut -c 3- | fzf \
+   Example: echo 'one\ntwo' | fzf | cat
+
+EOF
+    return 1
+  fi
+
+  case "$@" in
+  g)
+    # exit early if not git repo
+    if ! __jt__git-branch-name 1>/dev/null; then return 1; fi
+
+    # display current branch above header
+    fzff=$(git branch \
+      --format='%(refname:short)' \
+      --sort='-HEAD' |
+      fzf \
+        --header-lines=1 \
         --header 'git checkout' \
-        --preview "git show {} | ${preview}" \
-        --preview-window ${preview_window})
-    [ -n "$b" ] && git checkout "$b"
+        --preview "git show {} | $__jt__fzf_default_preview")
+    [ -n "$fzff" ] && git checkout "$fzff"
+    ;;
+  v)
+    fzff=$(fzf --header 'vim')
+    [ -f "$fzff" ] && vim "$fzff"
+    ;;
+  *)
+    # handle args/no args
+    if [ "$#" -ne 0 ]; then
+      #fzff=$(fzf --header "$@") # TODO "$@" is an array, not a string
+      fzff=$(fzf)
+      "$@" "$fzff"
+    else
+      fzf
+    fi
+    ;;
+  esac
+  unset fzff
 }
-else gf () {
-  git branch
-}
-fi
-
-if command -v fzf >/dev/null
-then vf () {
-    f=$(fzf \
-        --header 'vim' \
-        --preview "${preview} {}" \
-        --preview-window "${preview_window}")
-    [ -f "$f" ] && vim "$f"
-}
-else vf () {
-  vim .
-}
-fi
